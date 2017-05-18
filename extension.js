c0c7c747039d34736d9f5933743a027aba6de7cd
Vincent Bolduc <vincent.bolduc@gmail.com>
2017-05-17 21:39:55 -0400

    Added an icon to extention

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 const St = imports.gi.St;
 const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
 const Main = imports.ui.main;
+
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const RTCTransit = Me.imports.providers.rtcquebec;
 const DummyTransit = Me.imports.providers.dummy;
@@ -36,8 +38,15 @@ function updateDisplayedETA(transitResponse) {
     GLib.timeout_add(GLib.PRIORITY_DEFAULT, timer_rate, forceUpdate, null);
 
     // A bus icon could be nice to have
+    // An icon is available! It's name is 'route-transit-symbolic'
     let etaLabel = new St.Label({ text: transitResponse.lineNumber + ' : ' + transitResponse.estimatedTime + ' min'});
-    button.set_child(etaLabel);
+    let customIcon = Gio.icon_new_for_string(Me.path + '/icons/transit-symbolic.svg');
+    let busIcon = new St.Icon({ icon_size : 14, style_class: 'system-status-icon'});
+    busIcon.gicon = customIcon;
+    let box = new St.BoxLayout({ vertical: false });
+    box.add_actor(busIcon);
+    box.add_actor(etaLabel)
+    button.set_child(box);
 }
 
 function get_timer_refresh_rate(estimatedTimeLeft) {
